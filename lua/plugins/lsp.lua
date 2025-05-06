851c71ff570fd56a5b9796918493739c1f61aa8a
Eugen Scherbo <eugen.scherbo@myqops.com>
2025-05-06 18:29:31 +0300

    change gopls config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,21 +4,22 @@ return {{
         local lspconfig = require("lspconfig")
         local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-        lspconfig.gopls.setup({
-            capabilities = capabilities,
+			vim.lsp.config('gopls', {
+				capabilities = capabilities,
             settings = {
                 gopls = {
                     gofumpt = true,
-                    usePlaceholders = true
                 }
             }
-        })
 
+			})
+		
 			vim.lsp.config('omnisharp', {
 				capabilities = capabilities
 			})	
 
 			vim.lsp.enable('omnisharp')
+			vim.lsp.enable('gopls')
 
 				vim.keymap.set("n", "<leader>rr", vim.lsp.buf.rename, { desc = "LSP: Rename Symbol" })
     end
